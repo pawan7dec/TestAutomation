c5d03ff57eec109d70cf8b3ad27e256173bbb7e6
pawan7dec <pawan.vidy@gmail.com>
2017-07-12 07:40:50 +0530

    second commit

---
+++ Pattern5.java
@@ -0,0 +1,28 @@
+
+public class Pattern5 {
+	
+	public static void main(String[] args) {
+		
+		/*1 
+		1 2 
+		1 2 3 
+		1 2 3 4 
+		1 2 3 4 5 
+		1 2 3 4 5 6 
+		1 2 3 4 5 6 7 */
+		
+            for(int i=1;i<8;i++){
+			
+			for(int j=1;j<=i;j++){
+				
+				System.out.print(" ");
+				
+				
+			}
+			System.out.println("*");
+			System.out.println();
+			
+		}
+	}
+
+}
