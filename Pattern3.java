124428e8e7d886a9ea42652f8be75b9a660cc784
pawan7dec <pawan.vidy@gmail.com>
2017-07-12 07:40:41 +0530

    second commit

---
+++ Pattern3.java
@@ -0,0 +1,25 @@
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<8;i++){
+			
+			for(int j=1;j<=i;j++){
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+		
+		for(int m=6;m>=1;m--){
+			
+			for(int n=1;n<=m;n++){
+				
+				System.out.print(n+" ");
+			}
+			System.out.println();
+		}
+		
+		
+	}
+}
