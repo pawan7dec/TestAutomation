6299dbd97bfe4838d44042c06cbe42608d635eb1
pawan7dec <pawan.vidy@gmail.com>
2017-07-12 07:40:29 +0530

    second commit

---
+++ Pattern2.java
@@ -0,0 +1,31 @@
+
+public class Pattern2 {
+	
+	public static void main(String[] args) {
+		
+		
+		 for(int i=7;i>=1;i--){
+				
+				for(int j=1;j<=i;j++){
+					
+					System.out.print(" ");
+					
+				}
+				System.out.println("*");
+				System.out.println();
+				
+			}
+		 
+			for(int m=1;m<8;m++){
+				
+				for(int n=1;n<=m;n++){
+					
+					System.out.print(n+" ");
+					
+				}
+				System.out.println();
+				
+			}
+	}
+
+}
